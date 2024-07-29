85016814964be013cac4aba3923afe9d839b0ac1
jffitzpatrick <jf879@drexel.edu>
2024-07-29 16:03:28 -0400

    wrote starter SQL tables

--- setup.sql
+++ setup.sql
@@ -1,2 +1,38 @@
 CREATE DATABASE basicwars;
 \c basicwars
+CREATE TABLE games(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(15),
+	p1_id INTEGER,
+	p2_id INTEGER,
+	map_id INTEGER,
+	p1_units ARRAY,
+	p2_units ARRAY
+);
+
+CREATE TABLE unitTypes(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(15),
+	blurb VARCHAR (250),
+	armor INTEGER,
+	speed INTEGER,
+	flying BOOLEAN,
+	infantry BOOLEAN,
+	minrange INTEGER,
+	maxrange INTEGER,
+	sightrange INTEGER
+);
+
+CREATE TABLE units(
+	id SERIAL PRIMARY KEY,
+	type_id INTEGER,
+	game_id INTEGER,
+	player_id INTEGER,
+	pos_x INTEGER,
+	pos_y INTEGER,
+	cur_hp INTEGER,
+	capturing BOOLEAN,
+	capture_prog INTEGER
+);
+
+
